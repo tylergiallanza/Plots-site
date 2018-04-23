466f7abead89fa370b1fe0adb6cf0a594d223343
Tyler Giallanza <tylergiallanza@gmail.com>
2018-04-22 22:09:53 -0500

    Add ganalytics tracking and cname

--- pelicanconf.py
+++ pelicanconf.py
@@ -35,7 +35,7 @@ SOCIAL = (('You can add links in your config file', '#'),
 
 DEFAULT_PAGINATION = 10
 
-STATIC_PATHS = ['js','images','favicon.ico']
+STATIC_PATHS = ['js','images','favicon.ico', 'CNAME']
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
